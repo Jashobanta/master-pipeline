bf0ad153fe52c4c3eb1ca51ef6eb62f193cbe33e
Jashobanta <jashobanta.sarangi@gmail.com>
2025-09-05 04:49:32 +0000

    added filesgit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,28 @@
 locals {
-  repositories = length(trimspace(file("repositories.json"))) > 0 ? jsondecode(file("repositories.json")) : []
-  #repositories = jsondecode(file("repositories.json"))
-  target_repo_name = "projects/${var.project_id}/locations/${var.region}/connections/${var.github_connection_name}/repositories/${var.github_repo_owner}_${var.github_repo_name}"
-  repo_names = [for repo in local.repositories : repo.name]
-  repo_exists = contains(local.repo_names,local.target_repo_name)
+  repositories = jsondecode(file("${path.module}/"repositories.json"))
+  repo_map = {for repo in local.repositories : repo.name => repo }
 }
 
 resource "google_cloudbuildv2_repository" "linked_repo" {
-  count             = local.repo_exists ? 0 : 1
+  for_each = local.repo_map
+  #count             = local.repo_exists ? 0 : 1
   project           = var.project_id
   location          = var.region
-  name              = "${var.github_repo_owner}_${var.github_repo_name}"
+  name              = each.value.name
   parent_connection = var.github_connection_name
-  remote_uri        = "https://github.com/${var.github_repo_owner}/${var.github_repo_name}.git"
+  remote_uri        = each.value.remote_uri
 }
 
 resource "google_cloudbuild_trigger" "build_trigger" {
-  depends_on = [
-    google_cloudbuildv2_repository.linked_repo
-  ]
-  name        = var.trigger_name
-  description = var.description
+  for_each = google_cloudbuildv2_repository.linked_repo
+  name        = "auto-trigger-${each.key}"
+  description = "automated trigger for ${each.key}"
   location    = var.region
-  tags        = var.tags
   
   repository_event_config {
-    repository = local.repo_exists ? local.target_repo_name : google_cloudbuildv2_repository.linked_repo[0].id
+    repository = each.value.id
     push {
-      branch = var.branch_name
+      branch = each.value.defaultBranch
     }
   }
 
