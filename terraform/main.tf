a798db77d8ccbda20332965127c440e5ad1fa3a8
Jashobanta <jashobanta.sarangi@gmail.com>
2025-09-08 12:06:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,8 @@ resource "google_cloudbuild_trigger" "build_trigger" {
   service_account = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
 }
 
-resource "google_cloudbuild_trigger" "manual-trigger1" {
-  name        = "test-repo-manual-trigger1"
+resource "google_cloudbuild_trigger" "manual-trigger" {
+  name        = "test-repo-manual-trigger"
   location    = var.region
   repository_event_config {
     repository = google_cloudbuildv2_repository.linked_repo[0].id
